b18844359fdb15e3a9130d08b93fee7ec3cb08d3
UP_G (Andrey Churilov) <proga.ans@gmail.com>
2023-02-11 20:35:46 +0300

    увеличение длительности токенов

--- services/token.service.js
+++ services/token.service.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 
 class TokenService {
     generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '15s'})
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '15m'})
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '1d'})
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '15d'})
         return {
             accessToken,
             refreshToken
